03dc75abdbba47028f5ecdf4458884116c7452a3
Rustam-SH86 <rust-shafik@yandex.ru>
2026-04-13 16:33:32 +0400

    refactor modal,form,main,registration form

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+
+import './cards-color.js';
+import './homework-8.js';
+import './form.js';
+import './registration-form.js';
+import './modal.js';
---
+++ js/registration-form.js
@@ -0,0 +1,58 @@
+import { Modal } from './modal.js';
+import { Form } from './form.js';
+
+const overlay = document.querySelector('#overlay');
+const modalId = document.querySelector('#modal');
+const closeModalButton = document.querySelector('#closemodal-button');
+const registrationButton = document.querySelector('.registration-button');
+
+const modal = new Modal(modalId, overlay, closeModalButton);
+
+registrationButton.addEventListener('click', () => {
+  modal.openModal();
+});
+
+const subscribeForm = new Form('.subscribe-form');
+subscribeForm.form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  if (!subscribeForm.isValid()) {
+    console.log('Форма подписки невалидна');
+    return;
+  }
+
+  const data = subscribeForm.getFormValues();
+  console.log(data);
+
+  subscribeForm.resetForm();
+});
+
+const registrationForm = new Form('#registration-form');
+
+registrationForm.form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  if (!registrationForm.isValid()) {
+    console.log('Форма регистрации невалидна');
+    return;
+  }
+
+  const data = registrationForm.getFormValues();
+
+  if (data.password !== data.confirmPassword) {
+    console.log('Пароли не совпадают');
+    return;
+  }
+
+  const user = {
+    ...data,
+    password: btoa(data.password),
+    confirmPassword: btoa(data.confirmPassword),
+    createdOn: new Date(),
+  };
+
+  console.log('Регистрация успешна', user);
+
+  registrationForm.resetForm();
+  modal.closeModal();
+});
